159b253c27292dea943641fd698df711d2618ae1
Brandon Presley <brandon@presley.nz>
2025-02-11 23:14:04 +1300

    Make the cat stay upright!

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  double targetRotation = 0.0;
+  double currentRotation = 0.0;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 16), // ~60fps
+    )..addListener(() {
+        setState(() {
+          // Smooth lerp between current and target rotation
+          currentRotation = lerpDouble(currentRotation, targetRotation, 0.2) ??
+              currentRotation;
+        });
+      });
+
+    // Start continuous animation
+    _controller.repeat();
+
+    gyroscopeEventStream().listen((event) {
+      // Changed sign to + for correct rotation direction
+      targetRotation += event.z * 0.2;
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Transform.rotate(
+                angle: currentRotation,
+                child: Image.asset('assets/cat.jpg'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
