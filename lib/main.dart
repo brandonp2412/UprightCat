ac8595308395b06244c0cb821506c7310a705bf1
Brandon Presley <brandon@presley.nz>
2025-02-12 11:45:04 +1300

    Reduce code for minimal example

--- lib/main.dart
+++ lib/main.dart
@@ -31,38 +31,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
-  double targetRotation = 0.0;
-  double currentRotation = 0.0;
-  late AnimationController _controller;
+  double rotation = 0.0;
 
   @override
   void initState() {
     super.initState();
 
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 16), // ~60fps
-    )..addListener(() {
-        setState(() {
-          // Smooth lerp between current and target rotation
-          currentRotation = lerpDouble(currentRotation, targetRotation, 0.2) ??
-              currentRotation;
-        });
-      });
-
-    // Start continuous animation
-    _controller.repeat();
-
     gyroscopeEventStream().listen((event) {
-      // Changed sign to + for correct rotation direction
-      targetRotation += event.z * 0.2;
+      setState(() {
+        rotation += event.z * 0.2;
+      });
     });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
   }
 
   @override
@@ -79,7 +58,7 @@ class _MyHomePageState extends State<MyHomePage>
             SizedBox(
               height: 700,
               child: Transform.rotate(
-                angle: currentRotation,
+                angle: rotation,
                 child: Image.asset('assets/cat.jpg'),
               ),
             ),
